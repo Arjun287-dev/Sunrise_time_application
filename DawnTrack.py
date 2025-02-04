3e4504f0c0529b4229d0485c02c0abc3a642f328
Arjun Arundiyar <ramesharjun287@gmail.com>
2025-02-04 22:18:51 +0530

    final version with the api

--- DawnTrack.py
+++ DawnTrack.py
@@ -1,51 +1,51 @@
 import requests
 import streamlit as st
-from datetime import datetime, timezone, timedelta
+from datetime import datetime,timezone,timedelta
 
 def get_coordinates(city_name):
-    API_KEY = "API_KEY"
+    API_KEY = "4957901b5453a46183958e9be751a3d4"
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    
-    if response.status_code != 200 or not data:
-        st.error("Invalid response from the API or city not found.")
+    if response.status_code != 200:
+        print("Geocoding API not working.")
         return None
     return data[0]["name"]
 
-def fetch_weather_data(city_name):
-    API_KEY = "API_KEY"
+def fetch_sunrise_time(city_name):
+    API_KEY = "4957901b5453a46183958e9be751a3d4"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    
     if response.status_code != 200:
-        st.error("Invalid response from the API.")
+        print("Sunset time was not fetched. Please enter valid city name.")
         return None
     sunrise = data["sys"].get("sunrise")
-    timezone_offset = data.get("timezone", 0)
-    
-    return sunrise, timezone_offset
+    timezone_offset = data.get("timezone",0)
+    return sunrise,timezone_offset
 
-def convert_utc_to_local(utc_timestamp, timezone_offset):
-    utc_time = datetime.fromtimestamp(utc_timestamp, tz=timezone.utc)
+def utc_to_local_time(utc_timestamp,timezone_offset):
+    utc_time = datetime.fromtimestamp(utc_timestamp,tz=timezone.utc)
     local_time = utc_time + timedelta(seconds=timezone_offset)
-    return local_time.strftime('%I:%M:%S %p')
+    return local_time.strftime("%I:%M:%S %p")
 
 st.title("Sunrise Time")
-city = st.text_input("Enter city name")
+city = st.text_input("Enter the City Name")
 
 if st.button("Get Sunrise Time"):
-    if city:
+    try:
         city_name = get_coordinates(city)
-        if city_name:
-            sunrise_time, timezone_offset = fetch_weather_data(city_name)
-            if sunrise_time:
-                formatted_time = convert_utc_to_local(sunrise_time, timezone_offset)
-                st.subheader(f"Sunrise time in {city}: {formatted_time}")
-            else:
-                st.error("Could not fetch sunrise time. Please enter valid city name")
-        else:
-            st.error("Invalid city name.")
-    else:
-        st.error("Please enter a valid city name.")
+        try:
+            sunrise_time,timezone_offset = fetch_sunrise_time(city_name)
+            try:
+                formatted_time = utc_to_local_time(sunrise_time,timezone_offset)
+                st.subheader(f"The Sunset Time in {city_name} : {formatted_time}")
+            except:
+                st.error("Couldn't fetch the sunset time")
+        except:
+            st.error("Invalid city name")
+    except:
+        st.error("Please enter valid city name")
+
+
+        
